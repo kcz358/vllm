2a152d746c76f4ce9e3b4580d42eb8b85234d39e
kcz358 <kaichenzhang358@outlook.com>
2025-02-16 15:15:55 +0000

    Add build script

---
+++ scripts/build_python.sh
@@ -0,0 +1,7 @@
+
+export VLLM_COMMIT=6a854c7a2bb5b8a2015bbd83d94d311b991ac45d # use full commit hash from the main branch
+export VLLM_PRECOMPILED_WHEEL_LOCATION=https://wheels.vllm.ai/${VLLM_COMMIT}/vllm-1.0.0.dev-cp38-abi3-manylinux1_x86_64.whl
+python3 -m pip install --editable .
+
+python3 -m pip install transformers@git+https://github.com/huggingface/transformers
+python3 -m pip install hf_transfer
